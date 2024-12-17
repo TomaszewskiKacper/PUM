39d1295eb1906628399e7cdffc3dfa4c6b4c62a8
TomaszewskiKacper <94453117+TomaszewskiKacper@users.noreply.github.com>
2024-12-17 12:39:46 +0100

    4

---
+++ Lista4/app/src/main/java/com/example/lista4/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lista4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lista4.ui.theme.Lista4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column {
+                Text(
+                    text = "Pytanie 1/10",
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun quizScreen(){
+    
+}
